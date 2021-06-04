75e1da46742f2a048f3d0ebe5f113374ff08712f
Nik <nik.alekseev.spb@gmail.com>
2021-06-04 22:09:07 +0300

    Initial Commit

---
+++ ColorizedSwiftUIApp/ColorizedSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedSwiftUIAppApp.swift
+//  ColorizedSwiftUIApp
+//
+//  Created by MacBook on 04.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
